ad01b4a566aec8d8c8ddbbe15b0f0f832bdc4541
EdenEast <edenofest@gmail.com>
2023-02-22 16:36:04 -0500

    feat(compile): 3/4 baked

--- lua/fastcolor/init.lua
+++ lua/fastcolor/init.lua
@@ -2,9 +2,28 @@ local M = {}
 
 local root = vim.fn.stdpath("cache") .. "/colorcache"
 vim.opt.rtp:prepend(root)
-root = root .. "/colors/"
-if vim.fn.isdirectory(root) == 0 then
-	vim.fn.mkdir(root)
+local colors_root = root .. "/colors/"
+local cache_root = root .. "/cache/"
+
+local function ensure(path)
+  if vim.fn.isdirectory(path) == 0 then
+    vim.fn.mkdir(path, "p")
+  end
+end
+ensure(colors_root)
+ensure(cache_root)
+
+local function file_exists(name)
+  local f = io.open(name, "r")
+  return f ~= nil and io.close(f)
+end
+
+local function write(path, content)
+  local file = io.open(path, "wb")
+  if file then
+    file:write(content)
+    file:close()
+  end
 end
 
 local hl = vim.api.nvim_set_hl
@@ -13,68 +32,61 @@ local cache = {}
 local name
 
 local function inspect(t)
-	local list = {}
-	for k, v in pairs(t) do
-		local q = type(v) == "string" and [["]] or ""
-		table.insert(list, string.format([[%s = %s%s%s]], k, q, tostring(v), q))
-	end
-	return string.format([[{ %s }]], table.concat(list, ", "))
+  local list = {}
+  for k, v in pairs(t) do
+    local q = type(v) == "string" and [["]] or ""
+    table.insert(list, string.format([[%s = %s%s%s]], k, q, tostring(v), q))
+  end
+  return string.format([[{ %s }]], table.concat(list, ", "))
 end
 
-local path
+local exists
 
+vim.api.nvim_create_augroup("FastColor", { clear = true })
 vim.api.nvim_create_autocmd("ColorSchemePre", {
-	callback = function(O)
-		name = O.match
-		path = root .. name
-		local file = io.open(path .. ".vim", "wb")
-		if file then
-			file:write(string.format([[lua require("fastcolor").load("%s")]], name))
-			file:close()
-		end
-		vim.api.nvim_set_hl = function(id, group, opts)
-			if cache[name] == nil then
-				cache[name] = {
-					string.format(
-						[[
+  group = "FastColor",
+  callback = function(O)
+    name = O.match
+    local cache_path = cache_root .. name
+    exists = file_exists(cache_path)
+    if not exists then
+      vim.api.nvim_set_hl = function(id, group, opts)
+        if cache[name] == nil then
+          cache[name] = {
+            string.format(
+              [[
 return string.dump(function()
 if vim.g.colors_name then vim.cmd("hi clear") end
 vim.o.termguicolors = true
 vim.g.colors_name = "%s"
-vim.o.background = "dark"]],
-						name
-					),
-				}
-			end
-			table.insert(cache[name], string.format([[vim.api.nvim_set_hl(%s, "%s", %s)]], id, group, inspect(opts)))
-			hl(id, group, opts)
-			cnt = cnt + 1
-		end
-	end,
+vim.g.background = "dark"]],
+              name,
+              name
+            ),
+          }
+        end
+        table.insert(cache[name], string.format([[vim.api.nvim_set_hl(%s, "%s", %s)]], id, group, inspect(opts)))
+        hl(id, group, opts)
+        cnt = cnt + 1
+      end
+    end
+  end,
 })
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-	callback = function()
-		vim.api.nvim_set_hl = hl
-		print(path)
-		table.insert(cache[name], "end)")
+  group = "FastColor",
+  callback = function()
+    if not exists then
+      vim.api.nvim_set_hl = hl
+      table.insert(cache[name], "end)")
+      local cache_path = cache_root .. name
+      local colors_path = colors_root .. name .. ".vim"
 
-		local file = io.open(path, "wb")
-
-		if vim.g.fastcolor_debug then -- Debugging purpose
-			local deb = io.open(path .. ".lua", "wb")
-			deb:write(table.concat(cache[name], "\n"))
-			deb:close()
-		end
-
-		if file then
-			local f = loadstring(table.concat(cache[name], "\n"), "=")
-			file:write(f(), "\n")
-			file:close()
-		end
-		cache[name] = {}
-		cnt = 0
-	end,
+      write(cache_path .. ".lua", table.concat(cache[name], "\n"))
+      write(cache_path, loadstring(table.concat(cache[name], "\n"), "=")())
+      write(colors_path, string.format([[lua require("fastcolor").load("%s")]], name))
+    end
+  end,
 })
 
 function M.compile() end
@@ -82,19 +94,20 @@ function M.compile() end
 local lock = false -- Avoid g:colors_name reloading
 
 function M.load(colorscheme)
-	if lock then
-		return
-	end
-	local compiled_path = root .. colorscheme
-	lock = true
-	local f = loadfile(compiled_path)
-	if not f then
-		M.compile()
-		f = loadfile(compiled_path)
-	end
-	---@diagnostic disable-next-line: need-check-nil
-	f()
-	lock = false
+  if lock then
+    return
+  end
+  print("Loading from fastcolor: ", colorscheme)
+  local compiled_path = cache_root .. colorscheme
+  lock = true
+  local f = loadfile(compiled_path)
+  if not f then
+    M.compile()
+    f = loadfile(compiled_path)
+  end
+  ---@diagnostic disable-next-line: need-check-nil
+  f()
+  lock = false
 end
 
 return M
