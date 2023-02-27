06cc21075fa1d75f1fe97f45828ca17bbdef8692
nullchilly <nullchilly@gmail.com>
2023-02-27 10:30:01 +0700

    perf: remove default highlight groups from cache

--- plugin/colorcache.lua
+++ plugin/colorcache.lua
@@ -5,6 +5,16 @@ if vim.fn.isdirectory(colors_root) == 0 then vim.fn.mkdir(colors_root, "p") end
 
 vim.api.nvim_create_augroup("colorcache", { clear = true })
 
+local default_groups
+
+vim.api.nvim_create_autocmd("ColorSchemePre", {
+	group = "colorcache",
+	callback = function()
+		vim.cmd [[hi clear]]
+		default_groups = vim.api.nvim__get_hl_defs(0)
+	end,
+})
+
 vim.api.nvim_create_autocmd("ColorScheme", {
 	group = "colorcache",
 	callback = function(opts)
@@ -26,7 +36,7 @@ vim.g.colors_name = "%s"]],
 			}
 			local groups = vim.api.nvim__get_hl_defs(0)
 			for group, color in pairs(groups) do
-				if not color[true] then
+				if not (vim.inspect(color) == vim.inspect(default_groups[group])) and not color[true] then
 					table.insert(lines, string.format([[vim.api.nvim_set_hl(0, "%s", %s)]], group, vim.inspect(color)))
 				end
 			end
