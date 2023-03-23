b7538437ca5a5b37a1f6a62648fa8f014e32d1f0
nullchilly <nullchilly@gmail.com>
2023-03-23 17:28:06 +0700

    feat: use nvim_get_hl on nightly
    https://github.com/neovim/neovim/pull/22693

--- plugin/colorcache.lua
+++ plugin/colorcache.lua
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd("ColorSchemePre", {
 	group = "colorcache",
 	callback = function()
 		vim.cmd [[hi clear]]
-		default_groups = vim.api.nvim__get_hl_defs(0)
+		default_groups = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, {}) or vim.api.nvim__get_hl_defs(0)
 	end,
 })
 
@@ -34,10 +34,21 @@ vim.g.colors_name = "%s"]],
 					colors_name
 				),
 			}
-			local groups = vim.api.nvim__get_hl_defs(0)
-			for group, color in pairs(groups) do
-				if not (vim.inspect(color) == vim.inspect(default_groups[group])) and not color[true] then
-					table.insert(lines, string.format([[vim.api.nvim_set_hl(0, "%s", %s)]], group, vim.inspect(color)))
+			if vim.api.nvim_get_hl then
+				local groups = vim.api.nvim_get_hl(0, {})
+				for group, color in pairs(groups) do
+					local val = vim.inspect(color)
+					if not (val == vim.inspect(default_groups[group])) then
+						table.insert(lines, string.format([[vim.api.nvim_set_hl(0, "%s", %s)]], group, val))
+					end
+				end
+			else
+				local groups = vim.api.nvim__get_hl_defs(0)
+				for group, color in pairs(groups) do
+					local val = vim.inspect(color)
+					if not (val == vim.inspect(default_groups[group])) and not color[true] then
+						table.insert(lines, string.format([[vim.api.nvim_set_hl(0, "%s", %s)]], group, val))
+					end
 				end
 			end
 
